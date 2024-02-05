40ed8f0aa3587b6b99a7139987165f3840587b30
lynnagidza <lynnagidza@gmail.com>
2024-02-05 12:34:33 +0300

    webpack tasks 0 - 3

---
+++ 0x00-Webpack/README.md
@@ -0,0 +1,16 @@
+# Webpack
+[![nodejs](https://img.shields.io/badge/nodejs-v12.x.x-green.svg)](https://nodejs.org/en/)
+
+# Resources
+
+- [Wepback documentation](https://webpack.js.org/concepts/)
+- [Webpack beginner guide](https://www.sitepoint.com/webpack-beginner-guide/)
+- [npm-package.json](https://docs.npmjs.com/cli/v10/configuring-npm/package-json)
+
+# Learning Objectives
+
+- How to setup Webpack for a basic project
+- Entry points, output, and loaders
+- How to add plugins
+- How to split your code into chunks
+- How to setup a dev server
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[contenthash].[ext]',
+                            outputPath: '../assets',
+                        },
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                            },
+                            optipng: {
+                                optimizationLevel: 7,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4,
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../css/main.css',
+        }),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin(),
+        ],
+    },
+};
