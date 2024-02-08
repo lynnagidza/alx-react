249abf9ab9c80a386cc238bb31e142d3c6c2665e
lynnagidza <lynnagidza@gmail.com>
2024-02-08 13:30:43 +0300

    task 3 (part)

---
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -0,0 +1,4 @@
+import $ from 'jquery'
+
+$('header').append('<img src=""></img>');
+$('header').append('<h1>Holberton Dashboard</h1>');
---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        header: './modules/header/header.js',
+        body: './modules/body/body.js',
+        footer: './modules/footer/footer.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[contenthash].[ext]',
+                            outputPath: '../assets',
+                        },
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                            },
+                            optipng: {
+                                optimizationLevel: 7,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4,
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            inject: 'body',
+            chunks: ['header', 'body', 'footer'],
+        }),
+        new MiniCssExtractPlugin({
+            filename: '../css/main.css',
+        }),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin(),
+        ],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: 8564,
+    },
+};
